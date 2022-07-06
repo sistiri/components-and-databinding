3eab29efed1e97734e3ba4cc97b4a6cd226ca645
sistiri <sistiri@yahoo.com>
2022-07-06 23:07:46 +0200

    Getting Access to ng-content with @ContentChild

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChange, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChange, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -19,6 +19,7 @@ export class ServerElementComponent implements
   @Input('srvElement') element: {type: string, name: string, content: string}
   @Input() name: string
   @ViewChild('heading', {static: true}) header: ElementRef
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef
 
 
   constructor() {
@@ -33,6 +34,7 @@ export class ServerElementComponent implements
   ngOnInit(): void {
     console.log('ngOnInit called!')
     console.log('Text Content: ' + this.header.nativeElement.textContent)
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent)
   }
 
   ngDoCheck(): void {
@@ -41,6 +43,7 @@ export class ServerElementComponent implements
 
   ngAfterContentInit(): void {
     console.log('ngAfterContentInit called!')
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent)
   }
   ngAfterContentChecked(): void {
     console.log('ngAfterContentChecked called!')
